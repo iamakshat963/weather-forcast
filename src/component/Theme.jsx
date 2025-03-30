41dafe859f06ddd116ec3bcd8db5a5d66bf2778f
iamakshat963 <141138158+iamakshat963@users.noreply.github.com>
2025-03-30 18:26:09 +0530

    Update Theme.jsx

--- src/component/Theme.jsx
+++ src/component/Theme.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
-import './Theme.css'
+import './Theme.css' 
 
 const ThemeSet=()=>{
-    const [theme,setTheme]=useState(localStorage.getItem("theme") || "light");
+    const [theme,setTheme]=useState(localStorage.getItem("theme") || "light"); //set theme acording to preference
 
-    useEffect(()=>{
-        document.body.className=theme==="dark" ? "dark-theme":"";
+    useEffect(()=>{ // switching themes
+        document.body.className=theme==="dark" ? "dark-theme":""; 
         localStorage.setItem("theme",theme);
     },[theme]);
 
-    return(
+    return( //theme perference buttons
         <div className="theme">
-            <b>Theme:</b>
+            <b>Theme:</b> 
             <button className="light-btn" onClick={()=>setTheme("light")}>Light</button> 
             <b>/</b>
             <button className="dark-btn" onClick={()=>setTheme("dark")}>Dark</button>
